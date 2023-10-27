d456d201c9c51e782cfb6fe785c4a49bf4d8aa8c
Serega S <serega1067@yandex.ru>
2023-10-27 23:33:10 +0700

    Task homework 1 done

---
+++ JavaScriptAPIHomework1/data.js
@@ -0,0 +1,66 @@
+export const workouts = `
+[
+    {
+        "id": "1",
+        "name_of_workout": "Отжимания",
+        "time_of_workout": "09:30 - 11:00",
+        "max_number_of_participants": "15",
+        "current_number_of_participants": "5"
+    },
+    {
+        "id": "2",
+        "name_of_workout": "Присидания",
+        "time_of_workout": "11:00 - 11:45",
+        "max_number_of_participants": "10",
+        "current_number_of_participants": "3"
+    },
+    {
+        "id": "3",
+        "name_of_workout": "Подтягивания",
+        "time_of_workout": "11:00 - 11:50",
+        "max_number_of_participants": "20",
+        "current_number_of_participants": "18"
+    },
+    {
+        "id": "4",
+        "name_of_workout": "Прес",
+        "time_of_workout": "10:00 - 10:55",
+        "max_number_of_participants": "25",
+        "current_number_of_participants": "20"
+    },
+    {
+        "id": "5",
+        "name_of_workout": "Плаванье",
+        "time_of_workout": "11:00 - 11:45",
+        "max_number_of_participants": "10",
+        "current_number_of_participants": "8"
+    },
+    {
+        "id": "6",
+        "name_of_workout": "Эстафета",
+        "time_of_workout": "10:00 - 10:55",
+        "max_number_of_participants": "20",
+        "current_number_of_participants": "19"
+    },
+    {
+        "id": "7",
+        "name_of_workout": "Баскетбол",
+        "time_of_workout": "09:30 - 11:00",
+        "max_number_of_participants": "20",
+        "current_number_of_participants": "13"
+    },
+    {
+        "id": "8",
+        "name_of_workout": "Футбол",
+        "time_of_workout": "09:00 - 09:55",
+        "max_number_of_participants": "25",
+        "current_number_of_participants": "7"
+    },
+    {
+        "id": "9",
+        "name_of_workout": "Тенис",
+        "time_of_workout": "11:30 - 12:00",
+        "max_number_of_participants": "20",
+        "current_number_of_participants": "14"
+    }
+]`
